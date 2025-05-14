e4865e6b47c980b61ee03fed22e7d17d1dc9fbf7
felipe <felipelopes569lz@gmail.com>
2025-05-14 16:53:12 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade-4/SplashScreen.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Atividade_4
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            progressBar1 = new ProgressBar();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            progressBar1.Dock = DockStyle.Bottom;
+            progressBar1.Location = new Point(0, 224);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(582, 29);
+            progressBar1.Step = 1;
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(216, 54);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 116);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 250;
+            timer1.Tick += timer1_Tick;
+            // 
+            // SplashScreen
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DeepSkyBlue;
+            ClientSize = new Size(582, 253);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
+            Name = "SplashScreen";
+            Text = "SplashScreen";
+            Load += SplashScreen_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Atividade-4/SplashScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade_4
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+        private int progresso = 0; // variável para controlar o progresso da barra
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progresso += 2; // cada tick soma 2% (100% em 50 ticks × 50ms = 2,5s)
+            progressBar1.Value = progresso; // atualiza a barra de progresso
+
+            if (progresso >= 100 && timer1.Interval ==250)
+            {
+                timer1.Stop();
+                this.Hide();
+
+                LoginForm login = new LoginForm();
+                login.Show();
+            }
+        }
+    }
+}
